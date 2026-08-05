230c8d136586518ea6bf5078aede64371b701c2d
vetri5678 <vetriselvansaga@gmail.com>
2026-08-05 06:40:19 +0530

    Add solution for Sort Array by Increasing Frequency

---
+++ solutions/easy/Sort Array by Increasing Frequency.java
@@ -0,0 +1,17 @@
+// Title: Sort Array by Increasing Frequency
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/sort-array-by-increasing-frequency/
+
+            if(freq[i]>0){
+                if(bucket[freq[i]]==null){
+                    bucket[freq[i]]=new ArrayList<>();
+        List<Integer>[]bucket=new ArrayList[nums.length+1];
+        for(int i=0;i<=200;i++){
+        }
+        int arr[]=new int[nums.length];
+        for(int num:nums){
+            freq[num+100]++;
+class Solution {
+    public int[] frequencySort(int[] nums) {
+        int freq[]=new int[201];
