391704940114d2f27275002e2fcbd802cfbb2346
vetri5678 <vetriselvansaga@gmail.com>
2026-08-06 07:01:21 +0530

    Add solution for Minimum Index Sum of Two Lists

---
+++ solutions/easy/Minimum Index Sum of Two Lists.java
@@ -0,0 +1,19 @@
+// Title: Minimum Index Sum of Two Lists
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/minimum-index-sum-of-two-lists/
+
+                    minsum=sum;
+                 if(minsum>sum){
+                    result.clear();
+                    result.add(list2[i]);
+          for(int i=0;i<list2.length;i++){
+            if(map.containsKey(list2[i])){
+                 int sum=map.get(list2[i])+i;
+          int minsum=Integer.MAX_VALUE;
+          List<String>result=new ArrayList<>();
+          for(int i=0;i<list1.length;i++){
+            map.put(list1[i],i);
+          }
+        
+        HashMap<String,Integer>map=new HashMap<>();
