6d7d5cb69716ea0b0e1493bd666839418cd2eb69
vetri5678 <vetriselvansaga@gmail.com>
2026-08-07 05:53:11 +0530

    Add solution for Snapshot Array

---
+++ solutions/medium/Snapshot Array.java
@@ -0,0 +1,15 @@
+// Title: Snapshot Array
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/snapshot-array/
+
+    }
+        }
+            map[i].put(0,0);
+            map[i]= new TreeMap<>();
+    
+    public void set(int index, int val) {
+        map[index].put(snap_id,val);
+    }
+    
+    public int snap() {
