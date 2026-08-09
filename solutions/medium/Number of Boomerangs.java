bac5054f9c040ce945e2f94d457200d4a50764ca
vetri5678 <vetriselvansaga@gmail.com>
2026-08-09 15:51:56 +0530

    Add solution for Number of Boomerangs

---
+++ solutions/medium/Number of Boomerangs.java
@@ -0,0 +1,14 @@
+// Title: Number of Boomerangs
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/number-of-boomerangs/
+
+        }
+            }
+                count+=frequency*(frequency-1);
+            for(int frequency:map.values()){
+        
+            }
+                map.put(distance,map.getOrDefault(distance,0)+1);
+                int distance=dx*dx+dy*dy;
+                int dy=points[i][1]-points[j][1];
