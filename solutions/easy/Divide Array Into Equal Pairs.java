8fb4926131feee21361062dc72d340ab40ee654a
vetri5678 <vetriselvansaga@gmail.com>
2026-08-10 19:50:59 +0530

    Add solution for Divide Array Into Equal Pairs

---
+++ solutions/easy/Divide Array Into Equal Pairs.java
@@ -0,0 +1,16 @@
+// Title: Divide Array Into Equal Pairs
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/divide-array-into-equal-pairs/
+
+class Solution {
+    public boolean divideArray(int[] nums) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        for(int count:map.values()){
+            if(count%2!=0){
+        }
+                return false;
+            }
