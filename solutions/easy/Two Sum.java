67c5435a1e7b803bffe564d787d251ae51704e76
vetri5678 <vetriselvansaga@gmail.com>
2026-08-11 09:47:40 +0530

    Add solution for Two Sum

---
+++ solutions/easy/Two Sum.java
@@ -0,0 +1,22 @@
+// Title: Two Sum
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/two-sum/
+
+        for(int i=0;i<nums.length;i++){
+       
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+    }
+            for(int j=i+1;j<nums.length;j++){
+        }
+                if(nums[i]+nums[j]==target){
+            }
+                    return new int[]{i,j};
+                }
+}
+        return new int[]{};
+
+import java.util.Map;
+
+import java.util.HashMap;
