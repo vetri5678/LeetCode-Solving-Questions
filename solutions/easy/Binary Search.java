115d5a33b97f6925b89290c8f18166acee66666c
vetri5678 <vetriselvansaga@gmail.com>
2026-08-11 06:18:12 +0530

    Add solution for Binary Search

---
+++ solutions/easy/Binary Search.java
@@ -0,0 +1,18 @@
+// Title: Binary Search
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/binary-search/
+
+              }
+              else if(nums[mid]<target){
+              else if(nums[mid]>target){
+                  high=mid-1;
+              if (nums[mid]==target){
+                return mid;
+              }
+             while(low<=high){
+            int mid=(low+high)/2;
+    public int search(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+class Solution {
