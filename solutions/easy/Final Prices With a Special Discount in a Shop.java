09e724d14f7dd96b093a738a5fdc2f2d3019b597
vetri5678 <vetriselvansaga@gmail.com>
2026-08-11 06:41:47 +0530

    Add solution for Final Prices With a Special Discount in a Shop

---
+++ solutions/easy/Final Prices With a Special Discount in a Shop.java
@@ -0,0 +1,19 @@
+// Title: Final Prices With a Special Discount in a Shop
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/
+
+         
+            prices[index]-=prices[i];
+         while(!stack.isEmpty()&&prices[stack.peek()]>=prices[i]){
+            int index=stack.pop();
+        Stack<Integer>stack=new Stack<>();
+        for(int i=0;i<prices.length;i++){
+
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        }
+    }
+        stack.push(i);
+     return prices;
+}
