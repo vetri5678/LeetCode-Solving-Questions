e1d46ce96473fa748b3a04702b81ae1d0e22d9b7
vetri5678 <vetriselvansaga@gmail.com>
2026-08-13 22:19:16 +0530

    Add solution for Remove All Adjacent Duplicates In String

---
+++ solutions/easy/Remove All Adjacent Duplicates In String.java
@@ -0,0 +1,15 @@
+// Title: Remove All Adjacent Duplicates In String
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+
+       StringBuilder result=new StringBuilder();
+       for(char ch:stack){
+         result.append(ch);
+       }
+       return result.toString();
+    }
+}
+       }
+        }
+        stack.push(ch);
