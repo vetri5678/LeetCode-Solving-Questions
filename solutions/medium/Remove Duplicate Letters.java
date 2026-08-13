e5c64204155ec1cbcde2f002bf7426186be68074
vetri5678 <vetriselvansaga@gmail.com>
2026-08-13 06:23:25 +0530

    Add solution for Remove Duplicate Letters

---
+++ solutions/medium/Remove Duplicate Letters.java
@@ -0,0 +1,22 @@
+// Title: Remove Duplicate Letters
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/remove-duplicate-letters/
+
+                continue;
+            }
+          while(ans.length()>0 &&ans.charAt(ans.length()-1)>c&& count[ans.charAt(ans.
+          length()-1)-'a']>0){
+            visited[ans.charAt(ans.length()-1)-'a']=false;
+            ans.deleteCharAt(ans.length()-1);
+            if(visited[c-'a']){
+            count[c-'a']--;
+        for(char c:s.toCharArray()){
+        StringBuilder ans=new StringBuilder();
+        }
+            count[c-'a']++;
+        for(char c:s.toCharArray()){
+        boolean visited[]=new boolean[26];
+        int count[]=new int[26];
+    public String removeDuplicateLetters(String s) {
+class Solution {
