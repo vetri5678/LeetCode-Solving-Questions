147359108d73333170a0f19b4e049f637da83325
vetri5678 <vetriselvansaga@gmail.com>
2026-08-13 05:39:31 +0530

    Add solution for Asteroid Collision

---
+++ solutions/medium/Asteroid Collision.java
@@ -0,0 +1,21 @@
+// Title: Asteroid Collision
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/asteroid-collision/
+
+                }
+                else {
+                    alive = false;
+                }
+            }
+
+            if (alive) {
+                stack.push(asteroid);
+            }
+                    alive = false;
+                    stack.pop();
+                else if (stack.peek() == Math.abs(asteroid)) {
+                }
+                    stack.pop();
+                if (stack.peek() < Math.abs(asteroid)) {
+
