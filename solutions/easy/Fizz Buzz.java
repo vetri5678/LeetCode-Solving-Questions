7b6190bf31412d9d53713054b930f9f9c3cdb3f8
vetri5678 <vetriselvansaga@gmail.com>
2026-08-17 19:16:29 +0530

    Add solution for Fizz Buzz

---
+++ solutions/easy/Fizz Buzz.java
@@ -0,0 +1,19 @@
+// Title: Fizz Buzz
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/fizz-buzz/
+
+            list.add("Buzz");
+           else if(i%5==0){
+           }
+            list.add("Fizz");
+           if(i%5==0&&i%3==0){
+             list.add("FizzBuzz");
+           }
+           else if(i%3==0){
+           }
+           else{
+            list.add(Integer.toString(i));
+           }
+    }
+      return list;
