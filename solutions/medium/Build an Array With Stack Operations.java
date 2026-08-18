88ee66dd5ce855e2ab63192bcbe6a7f632b234b4
vetri5678 <vetriselvansaga@gmail.com>
2026-08-18 08:16:21 +0530

    Add solution for Build an Array With Stack Operations

---
+++ solutions/medium/Build an Array With Stack Operations.java
@@ -0,0 +1,14 @@
+// Title: Build an Array With Stack Operations
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/build-an-array-with-stack-operations/
+
+            else{
+              operation.add("Push");
+              operation.add("Pop");  
+              Currnum++;
+            }
+          }
+          return operation;
+    }
+}
