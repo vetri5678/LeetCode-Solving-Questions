10412300f2b335d8d09494d1dbc5947eda30c607
vetri5678 <vetriselvansaga@gmail.com>
2026-08-18 07:42:40 +0530

    Add solution for Validate Stack Sequences

---
+++ solutions/medium/Validate Stack Sequences.java
@@ -0,0 +1,18 @@
+// Title: Validate Stack Sequences
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/validate-stack-sequences/
+
+         while(!stack.isEmpty()&&stack.peek()==popped[j]){
+            j++;
+         }
+            stack.pop();
+         }
+       
+          
+            stack.push(num);
+         for(int num:pushed) {
+           int j=0;
+         Stack<Integer>stack=new Stack<>();
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+class Solution {
