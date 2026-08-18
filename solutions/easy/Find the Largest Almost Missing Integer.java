517f732aa415aa7fd2c20461d099035ea9824d69
vetri5678 <vetriselvansaga@gmail.com>
2026-08-18 06:48:18 +0530

    Add solution for Find the Largest Almost Missing Integer

---
+++ solutions/easy/Find the Largest Almost Missing Integer.java
@@ -0,0 +1,19 @@
+// Title: Find the Largest Almost Missing Integer
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/find-the-largest-almost-missing-integer/
+
+         ans=Math.max(ans,nums[0]);
+       }
+        if(map.get(nums[n-1])==1){
+         ans=Math.max(ans,nums[n-1]);
+       }
+       if(map.get(nums[0])==1){
+        }
+       }
+            ans=Math.max(ans,key);
+        for(int key:map.keySet()){
+       return ans;
+
+    }
+}
