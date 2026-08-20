1d5d40fff0ec641976d93e3ca57e47bb6b04b09e
vetri5678 <vetriselvansaga@gmail.com>
2026-08-20 07:54:47 +0530

    Add solution for Next Greater Node In Linked List

---
+++ solutions/medium/Next Greater Node In Linked List.java
@@ -0,0 +1,19 @@
+// Title: Next Greater Node In Linked List
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/next-greater-node-in-linked-list/
+
+        head=head.next;
+        list.add(head.val);
+      while(head!=null){
+class Solution {
+    public int[] nextLargerNodes(ListNode head) {
+      List<Integer> list=new ArrayList<>();
+ */
+ * }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode() {}
+ *     ListNode next;
+ *     int val;
+ * public class ListNode {
