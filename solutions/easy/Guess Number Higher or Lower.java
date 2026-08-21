268aa8b561a30b3e0c5901b03ebe65675a7f4d0b
vetri5678 <vetriselvansaga@gmail.com>
2026-08-21 06:47:56 +0530

    Add solution for Guess Number Higher or Lower

---
+++ solutions/easy/Guess Number Higher or Lower.java
@@ -0,0 +1,21 @@
+// Title: Guess Number Higher or Lower
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/guess-number-higher-or-lower/
+
+                return mid;
+            if(result==0){
+            int result=guess(mid);
+            }
+            else if(result==1){
+                low=mid+1;
+            }
+           
+             else{
+                high=mid-1;
+            int mid=low+(high-low)/2;
+        while(low<=high){
+        int high=n;
+        int low =1;
+    public int guessNumber(int n) {
+public class Solution extends GuessGame {
