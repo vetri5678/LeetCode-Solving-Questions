6256d7917c509fc8e728e4579149a79c8e365575
vetri5678 <vetriselvansaga@gmail.com>
2026-08-21 07:42:32 +0530

    Add solution for Valid Perfect Square

---
+++ solutions/easy/Valid Perfect Square.java
@@ -0,0 +1,20 @@
+// Title: Valid Perfect Square
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/valid-perfect-square/
+
+            }
+            else if(square<num){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+                return true;
+            if(square==num){
+            long square=(long)mid*mid;
+            int mid=left+(right-left)/2;
+        int right=num;
+        while(left<=right){
+        int left=1;
+class Solution {
+    public boolean isPerfectSquare(int num) {
