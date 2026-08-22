59c854b2478dd491ca70f308fbede802ebb82f2a
vetri5678 <vetriselvansaga@gmail.com>
2026-08-22 19:50:57 +0530

    Add solution for Find Peak Element

---
+++ solutions/medium/Find Peak Element.java
@@ -0,0 +1,18 @@
+// Title: Find Peak Element
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/find-peak-element/
+
+            if(nums[mid]<nums[mid+1]){
+        }
+                left=mid+1;
+            }
+            else {
+                 right=mid;
+            }
+            int mid=left+(right-left)/2;
+        return left;
+        int ans=-1;
+        while(left<right){
+    }
+}
