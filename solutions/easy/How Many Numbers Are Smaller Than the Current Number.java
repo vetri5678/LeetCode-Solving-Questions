45114a9e850410f0bd065aab7c0463ff252dc7a1
vetri5678 <vetriselvansaga@gmail.com>
2026-08-23 21:15:24 +0530

    Add solution for How Many Numbers Are Smaller Than the Current Number

---
+++ solutions/easy/How Many Numbers Are Smaller Than the Current Number.java
@@ -0,0 +1,15 @@
+// Title: How Many Numbers Are Smaller Than the Current Number
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+
+                }
+               
+                    count++;
+                if(nums[i]>nums[j]){
+            for(int j=0;j<nums.length;j++){
+            int count=0;
+        for(int i=0;i<nums.length;i++){
+        int ans[]=new int[nums.length];
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+class Solution {
