4ed6dcf54f8ec56532c1ce9771131624bf7dc284
vetri5678 <vetriselvansaga@gmail.com>
2026-08-27 13:52:31 +0530

    Add solution for Capacity To Ship Packages Within D Days

---
+++ solutions/medium/Capacity To Ship Packages Within D Days.java
@@ -0,0 +1,21 @@
+// Title: Capacity To Ship Packages Within D Days
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
+
+                        currentweight=0;
+                    }
+                    currentweight+=weight;
+                    if(currentweight+weight>mid){
+                        daysneeded++;
+                int daysneeded=1;
+                int currentweight=0;
+               for(int weight:weights){
+             }
+             while(low<high){
+                int mid=low+(high-low)/2;
+                low=Math.max(low,weight);
+                high+=weight;
+             int low=0;
+             int high=0;
+             for(int weight:weights){
