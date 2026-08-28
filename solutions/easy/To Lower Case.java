26119d5f54015c25f2b72c2da4eb3f712dac4d60
vetri5678 <vetriselvansaga@gmail.com>
2026-08-28 20:32:11 +0530

    Add solution for To Lower Case

---
+++ solutions/easy/To Lower Case.java
@@ -0,0 +1,10 @@
+// Title: To Lower Case
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/to-lower-case/
+
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
