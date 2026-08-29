4ecd2bb185ae523a37b0f6ba20c85ef6971279ab
vetri5678 <vetriselvansaga@gmail.com>
2026-08-29 19:53:57 +0530

    Add solution for Koko Eating Bananas

---
+++ solutions/medium/Koko Eating Bananas.java
@@ -0,0 +1,19 @@
+// Title: Koko Eating Bananas
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/koko-eating-bananas/
+
+            }
+            if(hours<=h){
+            for(int pile:piles){
+                hours+=(pile+mid-1)/mid;
+            long hours=0;
+            int mid=minspeed+(maxspeed-minspeed)/2;
+        while(minspeed<maxspeed){
+        }
+        for(int pile:piles){
+            maxspeed=Math.max(maxspeed,pile);
+        int minspeed=1;
+    public int minEatingSpeed(int[] piles, int h) {
+        int maxspeed=0;
+class Solution {
