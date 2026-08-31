9c06fa2fd7c4a6d2529e369d3ee4fb8dd41c0c21
vetri5678 <vetriselvansaga@gmail.com>
2026-08-31 20:41:06 +0530

    Add solution for Split Array Largest Sum

---
+++ solutions/hard/Split Array Largest Sum.java
@@ -0,0 +1,20 @@
+// Title: Split Array Largest Sum
+            // Difficulty: Hard
+            // Language: Java
+            // Link: https://leetcode.com/problems/split-array-largest-sum/
+
+            sum=num;
+        }
+        else{
+            sum+=num;
+        }
+       }
+            parts++;
+        if(sum+num>maxsum){
+       for(int num:nums){
+       int  sum=0;
+       int parts=1;
+    public boolean canpart(int nums[],int k,int maxsum){
+    }
+        return left;
+        }
