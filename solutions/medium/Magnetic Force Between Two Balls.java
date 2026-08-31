8d990d8205b09d6d6773f55bcfe4be566acc2827
vetri5678 <vetriselvansaga@gmail.com>
2026-08-31 19:45:31 +0530

    Add solution for Magnetic Force Between Two Balls

---
+++ solutions/medium/Magnetic Force Between Two Balls.java
@@ -0,0 +1,20 @@
+// Title: Magnetic Force Between Two Balls
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/magnetic-force-between-two-balls/
+
+            int mid=left+(right-left)/2;
+            if(canplace(position,m,mid)){
+                ans=mid;
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+        int right=position[position.length-1]-position[0];
+        int ans=0;
+        while(left<=right){
+            }
+        }
+        return ans;
+    }
+    public boolean canplace(int position[],int m,int distance){
