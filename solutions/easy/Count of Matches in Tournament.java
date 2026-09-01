6344f5a020ee3b1114be944be50015e9cbb40dca
vetri5678 <vetriselvansaga@gmail.com>
2026-09-01 21:34:00 +0530

    Add solution for Count of Matches in Tournament

---
+++ solutions/easy/Count of Matches in Tournament.java
@@ -0,0 +1,10 @@
+// Title: Count of Matches in Tournament
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/count-of-matches-in-tournament/
+
+class Solution {
+    public int numberOfMatches(int n) {
+        return n-1;
+    }
+}
